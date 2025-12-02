cb13b3b4924c1c8c351e91381f3687fa7d7c7a57
Victor Castro <victor.castro3002@gmail.com>
2025-12-01 23:02:48 -0600

    prueba2

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
 
       <div className="login-container">
         <div className="login-header">
-          <h2>BIENVENID DE NUEVO</h2>
+          <h2>BIENVENID DE NUEVO 2</h2>
           <p>Ingresa tus credenciales para continuar</p>
         </div>
 
