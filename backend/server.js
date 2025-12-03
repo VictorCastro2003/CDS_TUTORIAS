85c66cc2cf2a82480d1a9dd2d2795a7646175ce7
Victor Castro <victor.castro3002@gmail.com>
2025-12-02 20:59:57 -0600

    change in server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ async function start() {
     await sequelize.authenticate();
     console.log('DB connected ✅');
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on http://localhost:${PORT}`);
     });
   } catch (err) {
