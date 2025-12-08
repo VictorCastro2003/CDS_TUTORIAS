b891ed8a38f9df826995122154f626309db230b4
Victor Castro <victor.castro3002@gmail.com>
2025-12-08 11:06:22 -0600

    revision refresh token

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -2,10 +2,14 @@
 import express from 'express';
 import authController from '../controllers/authController.js';
 import { register, login } from '../validators/authValidator.js';
+import verifyToken from '../middlewares/verifyToken.js'; 
 
 const router = express.Router();
 
 router.post('/register', register, authController.register);
 router.post('/login', login, authController.login);
+router.post('/refresh', authController.refresh);        
+router.post('/logout', authController.logout);          
+router.post('/change-password', verifyToken, authController.changePassword); 
 
 export default router;
